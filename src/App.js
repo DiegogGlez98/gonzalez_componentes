2107e494fe3be25469250f5e068886599caa1eba
diegoglez <diegoglezh98@gmail.com>
2023-01-19 11:46:09 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>gonzalez herrera diego</h1>
+      <h1>practica compnentes</h1>
+      <Button href="https://utd.edu.mx/">utd</Button>
+      <Button href="https://github.com/">git</Button>
+      <Button href="https://twitter.com/?lang=es">twiter</Button>
     </div>
   );
 }
 
 export default App;
+
+
+
+
